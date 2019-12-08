2efc2006e9f2775494621b80af003f377c046c88
Cho Ying Chung <carmen@carmenbuMacBook-Pro.local>
2019-12-08 13:44:39 +0800

    1111

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -66,3 +66,5 @@ var onMapClickHandler = function(event) {
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
 }
+
+window.alert("Sorry! Still under construction");
